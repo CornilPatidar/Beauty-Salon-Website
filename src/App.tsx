5e95d69f523bdfdab745544e42dd9474759a2321
Cornil Patidar <patidarcornil@gmail.com>
2025-07-28 22:16:44 -0500

    Bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,21 @@ function App() {
       <Navbar />
       <Hero />
       <ContactButtons />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="min-h-screen flex items-center justify-center bg-pink-100">
+      <h1 className="text-4xl font-bold text-pink-600">
+        TAILWIND WORKS 🎉
+      </h1>
+    </div>
     </>
+    
   );
 }
 
