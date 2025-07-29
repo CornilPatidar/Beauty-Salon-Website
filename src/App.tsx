4e927fa6ce5562c29bc8564680be9ff7d1776906
Cornil Patidar <patidarcornil@gmail.com>
2025-07-28 22:21:51 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import ContactButtons from '../components/ContactButtons';
-
-function App() {
+export default function App() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <ContactButtons />
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className="min-h-screen flex items-center justify-center bg-pink-100">
-      <h1 className="text-4xl font-bold text-pink-600">
-        TAILWIND WORKS 🎉
+    <div className="min-h-screen flex items-center justify-center bg-pink-100">
+      <h1 className="text-4xl font-bold text-pink-600 font-sans">
+        If this is pink — Tailwind works.
       </h1>
     </div>
-    </>
-    
   );
 }
-
-export default App;
